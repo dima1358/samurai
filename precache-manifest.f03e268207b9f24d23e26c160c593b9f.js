08d0e8a040018fbe8043ef26b70b5d91dfb3db2d
dmitriykanitskiy <dmitriy.kanitskiy@gmail.com>
2019-11-11 22:09:35 +0200

    Updates

---
+++ precache-manifest.f03e268207b9f24d23e26c160c593b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a5648e509b4189b4d930da29df1be6",
+    "url": "/samurai/index.html"
+  },
+  {
+    "revision": "1723b56a6da0aaf71682",
+    "url": "/samurai/static/css/main.bdb336a9.chunk.css"
+  },
+  {
+    "revision": "ad4a54f3282eda0aac74",
+    "url": "/samurai/static/js/2.e0d73a75.chunk.js"
+  },
+  {
+    "revision": "1723b56a6da0aaf71682",
+    "url": "/samurai/static/js/main.9c58b732.chunk.js"
+  },
+  {
+    "revision": "88ec464bc346bb1f1dd3",
+    "url": "/samurai/static/js/runtime-main.b4e3aff3.js"
+  },
+  {
+    "revision": "20baaf3f8ba3375e960645b309807b63",
+    "url": "/samurai/static/media/preloader.20baaf3f.gif"
+  }
+]);

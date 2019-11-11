0e76533ea8fe8019bda15de7ec2fb38990fc078a
dmitriykanitskiy <dmitriy.kanitskiy@gmail.com>
2019-11-11 21:23:47 +0200

    first COMMIT

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import g from './General.module.css'
+import React, {Component} from 'react'
 import './App.css'
-import Header from './Header/Header.js'
-import Profile from './Profile/Profile.js'
-import Sidebar from './Sidebar/Sidebar.js'
+import Sidebar from './components/Sidebar/Sidebar.js'
+import {Route, withRouter} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import UsersContainer from "./components/Users/UsersContainer";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
+import {initializeApp} from "./redux/appReducer";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import Preloader from "./components/common/Preloader/Preloader";
 
+class App extends Component {
+    componentDidMount() {
+        this.props.initializeApp()
+    }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <div className='wrap'>
-        <Sidebar />
-        <Profile />
-      </div>
-    </div>
-  )
+    render() {
+        if (!this.props.initializeSuccess) {
+            return <Preloader/>
+        }
+        return (
+            <div>
+                <HeaderContainer/>
+                <div className='wrap'>
+                    <Sidebar/>
+                    <div className='content__wrap'>
+                        <Route path='/login' render={() => <Login/>}/>
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/news' render={() => <News/>}/>
+                        <Route path='/music' render={() => <Music/>}/>
+                        <Route path='/settings' render={() => <Settings/>}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App
+let MapStateToProps = state => ({
+    initializeSuccess: state.app.initializeSuccess
+})
+
+export default compose(
+    withRouter,
+    connect(MapStateToProps, {initializeApp}))(App)
